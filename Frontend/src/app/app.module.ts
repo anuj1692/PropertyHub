f25706b84d6abc2f825917a2fef13bdb765b4971
Anuj Sharma <anujsoftengg@gmail.com>
2022-07-16 00:57:01 +0530

    Add Routing

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -8,10 +8,14 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import{HttpClientModule} from '@angular/common/http';
 import { HousingService } from './services/housing.service';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
+import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
 
 const appRoutes: Routes = [
-  {path: '', component: PropertyCardComponent},
-  {path: 'add-property',component: AddPropertyComponent}
+  {path: '', component: PropertyListComponent},
+  {path: 'rent-property', component: PropertyListComponent},
+  {path: 'add-property',component: AddPropertyComponent},
+  {path: 'property-detail/:id',component: PropertyDetailComponent},
+  {path: '**',component: PropertyListComponent}
 ]
 
 @NgModule({
@@ -20,7 +24,8 @@ const appRoutes: Routes = [
     PropertyCardComponent,
     PropertyListComponent,
       NavBarComponent,
-      AddPropertyComponent
+      AddPropertyComponent,
+      PropertyDetailComponent
    ],
   imports: [
     BrowserModule,
